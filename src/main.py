5483290e866e823c035276f2e390d7e271f9cdda
Swve <bdswve@gmail.com>
2022-08-14 23:50:55 +0200

    ✨ init collections

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.routers import courses, users, auth, houses, orgs, roles
+from src.routers import collections, courses, users, auth, houses, orgs, roles
 from starlette.responses import FileResponse
 
 
@@ -13,3 +13,5 @@ global_router.include_router(houses.router, prefix="/houses", tags=["houses"])
 global_router.include_router(orgs.router, prefix="/orgs", tags=["orgs"])
 global_router.include_router(roles.router, prefix="/roles", tags=["roles"])
 global_router.include_router(courses.router, prefix="/courses", tags=["courses"])
+global_router.include_router(collections.router, prefix="/collections", tags=["collections"])
+
