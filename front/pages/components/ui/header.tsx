78577710311f6e8e55fa4bf072641bcdb8a54508
swve <bdswve@gmail.com>
2022-09-21 23:09:47 +0200

    feat: init login page

--- front/pages/components/ui/header.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Menu } from './menu'
-
-export const Header = () => {
-  return (
-    <div><Menu></Menu></div>
-  )
-}
---
+++ front/pages/signup.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/ui/layout";
+import { Title } from "../components/ui/styles/title";
+
+const SignUp = () => {
+  return <div>
+  <Layout>
+    <Title>Sign up </Title>
+  </Layout>
+</div>;
+};
+export default SignUp;
