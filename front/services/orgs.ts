1b3df392b73928013bc7829916f9b1c4f72157c4
swve <bdswve@gmail.com>
2022-09-25 10:00:53 +0200

    feat: add new org creation page

--- front/services/orgs.ts
+++ front/services/orgs.ts
@@ -15,11 +15,25 @@ export async function getUserOrganizations() {
     .catch((error) => console.log("error", error));
 }
 
-// export async function createNewOrganization(user_id) {}
+export async function createNewOrganization(body: any) {
+  const HeadersConfig = new Headers({ "Content-Type": "application/json" });
+
+  const requestOptions: any = {
+    method: "POST",
+    headers: HeadersConfig,
+    redirect: "follow",
+    credentials: "include",
+    body: JSON.stringify(body),
+  };
+
+  return fetch(`${getAPIUrl()}orgs/`, requestOptions)
+    .then((result) => result.json())
+    .catch((error) => console.log("error", error));
+}
 
 // export async function getOrganizationData(org_id) {}
 
-export async function deleteOrganizationFromBackend(org_id:any) {
+export async function deleteOrganizationFromBackend(org_id: any) {
   const HeadersConfig = new Headers({ "Content-Type": "application/json" });
 
   const requestOptions: any = {
