b11185510ba22ee99ae12a0ef7e90371cb804fc5
swve <bdswve@gmail.com>
2022-11-11 17:46:06 +0100

    feat: modal effects

--- front/components/modals/Modal.tsx
+++ front/components/modals/Modal.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import styled from "styled-components";
+import { motion, AnimatePresence } from "framer-motion";
 
 function Modal(props: any) {
   return (
     <div>
       <Overlay>
-        <Content>{props.children}</Content>
+        <AnimatePresence>
+          <motion.div
+            initial={{ opacity: 0, left: "50%", top: "50%", scale: 0.9, backdropFilter: "blur(10px)", y: -1, position: "absolute" }}
+            animate={{ opacity: 1, left: "50%", top: "50%", scale: 1, backdropFilter: "blur(10px)", y: 0, position: "absolute" }}
+            key="modal"
+            transition={{
+              type: "spring",
+              stiffness: 360,
+              damping: 70,
+              delay: 0.02,
+            }}
+            exit={{ opacity: 0, left: "50%", top: "46%", backdropFilter: "blur(10px)", y: -1, position: "absolute" }}
+          >
+            <Content>{props.children}</Content>
+          </motion.div>
+        </AnimatePresence>
       </Overlay>
     </div>
   );
@@ -17,7 +33,6 @@ const Overlay = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
   z-index: 100;
 `;
 
@@ -31,5 +46,6 @@ const Content = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  box-shadow: 0px 64px 84px 15px rgb(0 0 0 / 10%);
 `;
 export default Modal;
