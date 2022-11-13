5ae1e392923a64c76ced610a537c5d12eb118f14
swve <bdswve@gmail.com>
2022-11-13 22:33:05 +0100

    feat: save course chapters/elements

--- front/services/courses/chapters.ts
+++ front/services/courses/chapters.ts
@@ -20,6 +20,26 @@ export async function getCourseChaptersMetadata(course_id: any) {
   return data;
 }
 
+export async function updateChaptersMetadata(course_id: any, data: any) {
+  const HeadersConfig = new Headers({ "Content-Type": "application/json" });
+
+  const requestOptions: any = {
+    method: "PUT",
+    headers: HeadersConfig,
+    redirect: "follow",
+    credentials: "include",
+    body: JSON.stringify(data),
+  };
+
+  const result: any = await fetch(`${getAPIUrl()}chapters/meta/course_${course_id}`, requestOptions)
+    .then((result) => result.json())
+    .catch((error) => console.log("error", error));
+
+  console.log("result", result);
+  
+  return result;
+}
+
 export async function createChapter(data: any, course_id: any) {
   console.log("data", data, course_id);
   
