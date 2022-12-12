643d4ae2e734134b1f7e51cb52f60a0a3b0badf6
swve <bdswve@gmail.com>
2022-12-12 11:29:47 +0100

    feat: add element_id to file upload params

--- src/routers/files.py
+++ src/routers/files.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, Form
 from src.dependencies.auth import get_current_user
 from fastapi import HTTPException, status, UploadFile
 
@@ -10,31 +10,32 @@ router = APIRouter()
 
 
 @router.post("/picture")
-async def api_create_picture_file(file_object: UploadFile, current_user: PublicUser = Depends(get_current_user)):
+async def api_create_picture_file(file_object: UploadFile, element_id: str = Form(),  current_user: PublicUser = Depends(get_current_user)):
     """
     Create new picture file
     """
-    return await create_picture_file(file_object, "ed_123")
+    return await create_picture_file(file_object, element_id)
 
 
 @router.post("/video")
-async def api_create_video_file(file_object: UploadFile, current_user: PublicUser = Depends(get_current_user)):
+async def api_create_video_file(file_object: UploadFile,element_id: str = Form(), current_user: PublicUser = Depends(get_current_user)):
     """
     Create new video file
     """
-    return await create_video_file(file_object, "ed_123")
+    return await create_video_file(file_object, element_id)
+
 
 @router.get("/picture")
-async def api_get_picture_file(file_id :str ,current_user: PublicUser = Depends(get_current_user)):
+async def api_get_picture_file(file_id: str, current_user: PublicUser = Depends(get_current_user)):
     """
     Get picture file
     """
     return await get_picture_file(file_id, current_user)
 
+
 @router.get("/video")
-async def api_get_video_file(file_id :str ,current_user: PublicUser = Depends(get_current_user)):
+async def api_get_video_file(file_id: str, current_user: PublicUser = Depends(get_current_user)):
     """
     Get video file
     """
     return await get_video_file(file_id, current_user)
-    
