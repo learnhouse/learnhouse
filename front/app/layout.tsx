f7325e794491a371989a488ed871d2e42d0b35de
swve <bdswve@gmail.com>
2023-01-13 18:45:56 +0100

    feat: init element ui design

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,12 +1,12 @@
+
 import "../styles/globals.css";
 import StyledComponentsRegistry from "../services/lib/styled-registry";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html className="" lang="en">
       <head />
       <body>
-        {" "}
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </body>
     </html>
