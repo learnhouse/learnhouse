88dc060bae32dd62f5224ffca70629ded1f05f60
swve <bdswve@gmail.com>
2023-01-14 01:36:54 +0100

    fix: layout updates

---
+++ front/app/_orgs/[orgslug]/template.tsx
@@ -0,0 +1,25 @@
+"use client";
+import { motion } from "framer-motion";
+export default function Template({ children }: { children: React.ReactNode }) {
+   
+  const variants = {
+    hidden: { opacity: 0, x: 0, y: 0 },
+    enter: { opacity: 1, x: 0, y: 0 },
+    exit: { opacity: 0, x: 0, y: 0 },
+  };
+
+  return (
+    <div>
+      <motion.main
+        variants={variants} // Pass the variant object into Framer Motion
+        initial="hidden" // Set the initial state to variants.hidden
+        animate="enter" // Animated state to variants.enter
+        exit="exit" // Exit state (used later) to variants.exit
+        transition={{ type: "linear" }} // Set the transition to linear
+        className=""
+      >
+        {children}
+      </motion.main>
+    </div>
+  );
+}
--- front/app/signup/page.tsx
+++ front/app/signup/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { Header } from "../../components/UI/Header";
 import Layout from "../../components/UI/Layout";
