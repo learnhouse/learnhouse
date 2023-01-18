a91fc6e3192a02d3f4e0d1f40163073b2db08ecc
swve <bdswve@gmail.com>
2023-01-18 20:07:51 +0100

    chore: refactor files & request for db instance

--- src/services/houses.py
+++ src/services/houses.py
@@ -3,7 +3,7 @@ from typing import List
 from uuid import uuid4
 from pydantic import BaseModel
 from src.services.users import PublicUser, User
-from src.services.database import create_config_collection, check_database, create_database, learnhouseDB, learnhouseDB
+from src.services.database import  check_database, create_database, learnhouseDB, learnhouseDB
 from src.services.security import *
 from fastapi import FastAPI, HTTPException, status, Request, Response, BackgroundTasks
 from datetime import datetime
