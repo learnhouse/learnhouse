7ff79318215159a2432ba5e5aa1b443eec55faad
swve <bdswve@gmail.com>
2023-01-24 22:31:38 +0100

    feat: use swr for collections

--- front/services/collections.ts
+++ front/services/collections.ts
@@ -1,41 +1,9 @@
 import { getAPIUrl } from "./config";
 
-export async function getOrgCollections(org_slug: any) {
-  const HeadersConfig = new Headers({ "Content-Type": "application/json" });
-
-  const requestOptions: any = {
-    method: "GET",
-    headers: HeadersConfig,
-    redirect: "follow",
-    credentials: "include",
-  };
-
-  return fetch(`${getAPIUrl()}collections/page/1/limit/10`, requestOptions)
-    .then((result) => result.json())
-    .catch((error) => console.log("error", error));
-}
-
-export async function getCollection(collection_slug: any) {
-  const HeadersConfig = new Headers({ "Content-Type": "application/json" });
-
-  const requestOptions: any = {
-    method: "GET",
-    headers: HeadersConfig,
-    redirect: "follow",
-    credentials: "include",
-  };
-
-  return fetch(
-    `${getAPIUrl()}collections/${collection_slug}`,
-    requestOptions
-  )
-    .then((result) => result.json())
-    .catch((error) => console.log("error", error));
-}
-
-
-
-
+/*
+ This file includes only POST, PUT, DELETE requests
+ GET requests are called from the frontend using SWR (https://swr.vercel.app/)
+*/
 
 export async function deleteCollection(collection_id: any) {
   const HeadersConfig = new Headers({ "Content-Type": "application/json" });
