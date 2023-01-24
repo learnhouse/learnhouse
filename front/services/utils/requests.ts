c94a8bf3649f769e13e88adcc22461316bb8c2fd
swve <bdswve@gmail.com>
2023-01-24 20:59:25 +0100

    feat: init swr usage

--- front/services/utils/requests.ts
+++ front/services/utils/requests.ts
@@ -12,3 +12,31 @@ export const RequestBody = (method: string, data: any) => {
   return options;
 };
 
+export const swrFetcher = async (url: string, body: any) => {
+
+  // Create the request options
+  let HeadersConfig = new Headers({ "Content-Type": "application/json" });
+  let options: any = {
+    method: "GET",
+    headers: HeadersConfig,
+    redirect: "follow",
+    credentials: "include",
+  };
+
+  // If there is a body, add it to the request options
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  // Fetch the data
+  const res = await fetch(url, options);
+
+  // If the response is not in the 200 range, throw an error
+  if (!res.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    throw error;
+  }
+
+  // Return the data
+  return res.json();
+};
