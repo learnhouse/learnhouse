583dc9c4529b3160fe8012f25425de0583253d33
swve <bdswve@gmail.com>
2023-01-29 19:15:10 +0100

    feat: use swr for course edit

--- front/app/_orgs/[orgslug]/course/[courseid]/lecture/[lectureid]/edit/page.tsx
+++ front/app/_orgs/[orgslug]/course/[courseid]/lecture/[lectureid]/edit/page.tsx
@@ -7,44 +7,24 @@ import { useRouter } from "next/navigation";
 import { getLecture } from "../../../../../../../../services/courses/lectures";
 import AuthProvider from "../../../../../../../../components/Security/AuthProvider";
 import EditorWrapper from "../../../../../../../../components/Editor/EditorWrapper";
-import { getCourseMetadata } from "../../../../../../../../services/courses/courses";
+import useSWR, { mutate } from "swr";
+import { getAPIUrl } from "@services/config";
+import { swrFetcher } from "@services/utils/requests";
 
 
 function EditLecture(params: any) {
   const router = useRouter();
   const lectureid = params.params.lectureid;
   const courseid = params.params.courseid;
-  const [lecture, setLecture] = React.useState<any>({});
-  const [courseInfo, setCourseInfo] = React.useState({}) as any;
-  const [isLoading, setIsLoading] = React.useState(true);
+  const { data: courseInfo, error: error_course } = useSWR(`${getAPIUrl()}courses/meta/course_${courseid}`, swrFetcher);
+  const { data: lecture, error: error_lecture } = useSWR(`${getAPIUrl()}lectures/lecture_${lectureid}`, swrFetcher);
 
-  async function fetchLectureData() {
-    const lecture = await getLecture("lecture_" + lectureid);
-    setLecture(lecture);
-  }
 
-  async function fetchCourseInfo() {
-    const course = await getCourseMetadata("course_" + courseid);
-    setCourseInfo(course);
-  }
-
-  async function fetchAllData() {
-    await fetchLectureData();
-    await fetchCourseInfo();
-    setIsLoading(false);
-  }
-
-  React.useEffect(() => {
-    if (lectureid && courseid) {
-      fetchAllData();
-    }
-    return () => {};
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [lectureid, courseid ]);
+  
 
   return (
     <AuthProvider>
-      {isLoading ? <div>Loading...</div> : <EditorWrapper course={courseInfo} lecture={lecture} content={lecture.content}></EditorWrapper>}
+      {!courseInfo || !lecture  ? <div>Loading...</div> : <EditorWrapper course={courseInfo} lecture={lecture} content={lecture.content}></EditorWrapper>}
     </AuthProvider>
   );
 }
