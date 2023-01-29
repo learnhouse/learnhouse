8b0289b8b513982f006c8520df39e4a819d154b6
swve <bdswve@gmail.com>
2023-01-29 19:01:48 +0100

    fix: menu links issue

--- front/services/config.ts
+++ front/services/config.ts
@@ -4,3 +4,12 @@ const LEARNHOUSE_BACKEND_URL = "http://localhost:1338/";
 export const getAPIUrl = () => LEARNHOUSE_API_URL;
 
 export const getBackendUrl = () => LEARNHOUSE_BACKEND_URL;
+
+export const getUriWithOrg = ( orgslug: string, path: string) => {
+  return `http://localhost:3000/org/${orgslug}${path}`;
+};
+
+export const getOrgFromUri = (uri: any) => {
+  let org = uri.match(/\/org\/(.*?)\//)[1];
+  return org;
+};
