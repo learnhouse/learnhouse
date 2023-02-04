72dd71d50c8c7c30319d47b184bd2bed230aee59
swve <bdswve@gmail.com>
2023-02-04 16:32:29 +0100

    chore: use RequestBody for everything else

--- front/services/files/video.ts
+++ front/services/files/video.ts
@@ -1,38 +1,19 @@
 import { getAPIUrl } from "@services/config";
+import { RequestBody, RequestBodyForm } from "@services/utils/requests";
 
 export async function uploadNewVideoFile(file: any, lecture_id: string) {
-  const HeadersConfig = new Headers();
-
   // Send file thumbnail as form data
   const formData = new FormData();
   formData.append("file_object", file);
   formData.append("lecture_id", lecture_id);
 
-  const requestOptions: any = {
-    method: "POST",
-    headers: HeadersConfig,
-    redirect: "follow",
-    credentials: "include",
-    body: formData,
-  };
-
-  return fetch(`${getAPIUrl()}files/video`, requestOptions)
+  return fetch(`${getAPIUrl()}files/video`, RequestBodyForm("POST", formData))
     .then((result) => result.json())
     .catch((error) => console.log("error", error));
 }
 
 export async function getVideoFile(file_id: string) {
-  const HeadersConfig = new Headers({ "Content-Type": "application/json" });
-
-  const requestOptions: any = {
-    method: "GET",
-    headers: HeadersConfig,
-    redirect: "follow",
-    credentials: "include",
-  };
-
-  
-  return fetch(`${getAPIUrl()}files/video?file_id=${file_id}`, requestOptions)
+  return fetch(`${getAPIUrl()}files/video?file_id=${file_id}`, RequestBody("GET", null))
     .then((result) => result.json())
     .catch((error) => console.log("error", error));
 }
