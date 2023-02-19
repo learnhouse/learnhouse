f48b77631302495b88aac9f2f946a4e9113138a3
swve <bdswve@gmail.com>
2023-02-19 23:41:27 +0100

    chore: re-order api routes

--- src/routers/blocks.py
+++ src/routers/blocks.py
@@ -18,14 +18,6 @@ async def api_create_image_file_block(request: Request, file_object: UploadFile,
     return await create_image_file(request, file_object, lecture_id)
 
 
-@router.post("/video")
-async def api_create_video_file_block(request: Request, file_object: UploadFile, lecture_id: str = Form(), current_user: PublicUser = Depends(get_current_user)):
-    """
-    Create new video file
-    """
-    return await create_video_file(request, file_object, lecture_id)
-
-
 @router.get("/image")
 async def api_get_image_file_block(request: Request, file_id: str, current_user: PublicUser = Depends(get_current_user)):
     """
@@ -33,6 +25,12 @@ async def api_get_image_file_block(request: Request, file_id: str, current_user:
     """
     return await get_image_file(request, file_id, current_user)
 
+@router.post("/video")
+async def api_create_video_file_block(request: Request, file_object: UploadFile, lecture_id: str = Form(), current_user: PublicUser = Depends(get_current_user)):
+    """
+    Create new video file
+    """
+    return await create_video_file(request, file_object, lecture_id)
 
 @router.get("/video")
 async def api_get_video_file_block(request: Request, file_id: str, current_user: PublicUser = Depends(get_current_user)):
@@ -42,20 +40,20 @@ async def api_get_video_file_block(request: Request, file_id: str, current_user:
     return await get_video_file(request, file_id, current_user)
 
 
-@router.get("/document")
-async def api_get_document_file_block(request: Request, file_id: str, current_user: PublicUser = Depends(get_current_user)):
-    """
-    Get document file
-    """
-    return await get_document_file(request, file_id, current_user)
-
-
 @router.post("/document")
 async def api_create_document_file_block(request: Request, file_object: UploadFile, lecture_id: str = Form(), current_user: PublicUser = Depends(get_current_user)):
     """
     Create new document file
     """
     return await create_document_file(request, file_object, lecture_id)
+
+
+@router.get("/document")
+async def api_get_document_file_block(request: Request, file_id: str, current_user: PublicUser = Depends(get_current_user)):
+    """
+    Get document file
+    """
+    return await get_document_file(request, file_id, current_user)
 
 
 @router.post("/quiz")
@@ -72,6 +70,7 @@ async def api_get_quiz_options(request: Request, block_id: str, current_user: Pu
     Get quiz options
     """
     return await get_quiz_block_options(request, block_id, current_user)
+
 
 @router.get("/quiz/answers")
 async def api_get_quiz_answers(request: Request, block_id: str, current_user: PublicUser = Depends(get_current_user)):
