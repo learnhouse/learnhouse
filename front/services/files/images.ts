0b2e407bf3ff1e4b9b6c4989af25ee328c25abc5
swve <bdswve@gmail.com>
2023-02-19 23:37:47 +0100

    chore: refactor block files

--- front/services/files/images.ts
+++ front/services/files/images.ts
@@ -7,14 +7,14 @@ export async function uploadNewImageFile(file: any, lecture_id: string) {
   formData.append("file_object", file);
   formData.append("lecture_id", lecture_id);
 
-  return fetch(`${getAPIUrl()}files/picture`, RequestBodyForm("POST", formData))
+  return fetch(`${getAPIUrl()}blocks/image`, RequestBodyForm("POST", formData))
     .then((result) => result.json())
     .catch((error) => console.log("error", error));
 }
 
 export async function getImageFile(file_id: string) {
   // todo : add course id to url
-  return fetch(`${getAPIUrl()}files/picture?file_id=${file_id}`, RequestBody("GET", null))
+  return fetch(`${getAPIUrl()}blocks/image?file_id=${file_id}`, RequestBody("GET", null))
     .then((result) => result.json())
     .catch((error) => console.log("error", error));
 }
