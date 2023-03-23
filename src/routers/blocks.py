3d028c6fdabdc5232a05c4a8897e57266b2ee64d
swve <bdswve@gmail.com>
2023-03-23 22:03:05 +0100

    feat: refactor imageBlock

--- src/routers/blocks.py
+++ src/routers/blocks.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends, UploadFile, Form, Request
 from src.dependencies.auth import get_current_user
 from fastapi import HTTPException, status, UploadFile
-from src.services.blocks.imageBlock.images import create_image_file, get_image_file
-from src.services.blocks.videoBlock.videos import create_video_file, get_video_file
-from src.services.blocks.pdfBlock.documents import create_document_file, get_document_file
-from src.services.blocks.quizBlock.quizBlock import create_quiz_block, get_quiz_block_answers, get_quiz_block_options, quizBlock
+from src.services.blocks.block_types.imageBlock.images import create_image_file, get_image_file
+from src.services.blocks.block_types.videoBlock.videoBlock import create_video_block, get_video_block
+from src.services.blocks.block_types.pdfBlock.documents import create_document_file, get_document_file
+from src.services.blocks.block_types.quizBlock.quizBlock import create_quiz_block, get_quiz_block_answers, get_quiz_block_options, quizBlock
 from src.services.users.users import PublicUser
 
 router = APIRouter()
@@ -25,19 +25,21 @@ async def api_get_image_file_block(request: Request, file_id: str, current_user:
     """
     return await get_image_file(request, file_id, current_user)
 
+
 @router.post("/video")
 async def api_create_video_file_block(request: Request, file_object: UploadFile, lecture_id: str = Form(), current_user: PublicUser = Depends(get_current_user)):
     """
     Create new video file
     """
-    return await create_video_file(request, file_object, lecture_id)
+    return await create_video_block(request, file_object, lecture_id)
+
 
 @router.get("/video")
 async def api_get_video_file_block(request: Request, file_id: str, current_user: PublicUser = Depends(get_current_user)):
     """
     Get video file
     """
-    return await get_video_file(request, file_id, current_user)
+    return await get_video_block(request, file_id, current_user)
 
 
 @router.post("/document")
