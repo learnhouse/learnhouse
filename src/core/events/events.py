123e25a87c52cf40937cbda2d2419875be7bccfc
swve <bdswve@gmail.com>
2023-03-24 22:50:08 +0100

    feat: use yaml and envs to load config

--- src/core/events/events.py
+++ src/core/events/events.py
@@ -1,12 +1,16 @@
 from typing import Callable
 from fastapi import FastAPI
 from src.core.events.database import close_database, connect_to_db
+from src.core.events.logs import create_logs_dir
 
 
 def startup_app(app: FastAPI) -> Callable:
     async def start_app() -> None:
         # Connect to database
         await connect_to_db(app)
+
+        # Create logs directory
+        await create_logs_dir()
 
     return start_app
 
