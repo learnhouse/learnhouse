716c6c163edea104466d5ff5888f2b8a6fab3b4e
swve <bdswve@gmail.com>
2023-03-24 23:58:09 +0100

    feat: improve middleware implementation

--- front/services/config/config.ts
+++ front/services/config/config.ts
@@ -5,14 +5,31 @@ export const getAPIUrl = () => LEARNHOUSE_API_URL;
 
 export const getBackendUrl = () => LEARNHOUSE_BACKEND_URL;
 
+export const getSelfHostedOption = () => false;
+
 export const getUriWithOrg = (orgslug: string, path: string) => {
+  const selfHosted = getSelfHostedOption();
+  if (selfHosted) {
+    return `http://localhost:3000${path}`;
+  }
   return `http://${orgslug}.localhost:3000${path}`;
 };
 
 export const getOrgFromUri = () => {
-  const hostname = window.location.hostname;
-  // get the orgslug from the hostname
-  const orgslug = hostname.split(".")[0];
-  return orgslug;
-  
+  const selfHosted = getSelfHostedOption();
+  if (selfHosted) {
+    getDefaultOrg();
+  } else {
+    const hostname = window.location.hostname;
+    // get the orgslug from the hostname
+    const orgslug = hostname.split(".")[0];
+    return orgslug;
+  }
+};
+
+export const getDefaultOrg = () => {
+  const selfHosted = getSelfHostedOption();
+  if (selfHosted) {
+    return "test";
+  }
 };
