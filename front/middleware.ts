aa90283fe77eb4680cead357eb63071c518ade32
swve <bdswve@gmail.com>
2023-03-24 23:58:09 +0100

    feat: improve middleware implementation

--- front/middleware.ts
+++ front/middleware.ts
@@ -1,3 +1,4 @@
+import { getDefaultOrg, getSelfHostedOption } from "@services/config/config";
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
@@ -16,41 +17,30 @@ export const config = {
 
 export default function middleware(req: NextRequest) {
   const url = req.nextUrl;
-
-  // Get hostname of request (e.g. demo.vercel.pub, demo.localhost:3000)
+  const isSelfHosted = getSelfHostedOption();
   const hostname = req.headers.get("host") || "learnhouse.app";
+  let currentHost = hostname.replace(".localhost:3000", "");
+  
+  if (!isSelfHosted && currentHost === ("localhost:3000")) {
+    // Redirect to error page if not self-hosted and on localhost
+    const errorUrl = "/error";
+    return NextResponse.redirect(errorUrl, { status: 302 });
+  }
 
-  /*  You have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
-      You can also use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      in this case, our team slug is "platformize", thus *.platformize.vercel.app works. Do note that you'll
-      still need to add "*.platformize.vercel.app" as a wildcard domain on your Vercel dashboard. */
-  let currentHost =
-    process.env.NODE_ENV === "production" && process.env.VERCEL === "1"
-      ? hostname.replace(`.vercel.pub`, "").replace(`.platformize.vercel.app`, "")
-      : hostname.replace(`.localhost:3000`, "");
-
-  /*  Editor route */
   if (url.pathname.match(/^\/course\/[^/]+\/activity\/[^/]+\/edit$/)) {
     url.pathname = `/_editor${url.pathname}`;
-    console.log("editor route", url.pathname);
-
     return NextResponse.rewrite(url, { headers: { orgslug: currentHost } });
   }
 
-  /*  Organizations route */
   if (url.pathname.startsWith("/organizations")) {
-    url.pathname = url.pathname.replace("/organizations", `/organizations${currentHost}`);
-    // remove localhost:3000 from url
-    url.pathname = url.pathname.replace(`localhost:3000`, "");
-    
-    
+    url.pathname = url.pathname.replace("/organizations", `/organizations${currentHost}`).replace("localhost:3000", "");
     return NextResponse.rewrite(url);
   }
 
-  console.log("currentHost", url);
+  if (isSelfHosted) {
+    currentHost = getDefaultOrg() || currentHost;
+  }
 
-  // rewrite everything else to `/_sites/[site] dynamic route
   url.pathname = `/_orgs/${currentHost}${url.pathname}`;
-
-  return NextResponse.rewrite(url, { headers: { olgslug: currentHost } });
+  return NextResponse.rewrite(url, { headers: { orgslug: currentHost } });
 }
