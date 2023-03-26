e14ba02f97582f0cb2bc3cf2662cf617aca0c365
swve <bdswve@gmail.com>
2023-03-26 18:46:03 +0200

    feat: introduce trail api and depreciate old api

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from src.routers import activity, blocks, users, auth, orgs, roles
+from src.routers import blocks, trail, users, auth, orgs, roles
 from src.routers.courses import chapters, collections, courses,activities
 
 
@@ -16,6 +16,6 @@ global_router.include_router(courses.router, prefix="/courses", tags=["courses"]
 global_router.include_router(chapters.router, prefix="/chapters", tags=["chapters"])
 global_router.include_router(activities.router, prefix="/activities", tags=["activities"])
 global_router.include_router(collections.router, prefix="/collections", tags=["collections"])
-global_router.include_router(activity.router, prefix="/activity", tags=["activity"])
+global_router.include_router(trail.router, prefix="/trail", tags=["trail"])
 
 
---
+++ src/services/trail.py
@@ -0,0 +1,205 @@
+from cmath import log
+from datetime import datetime
+import json
+from typing import List, Literal, Optional
+from uuid import uuid4
+from fastapi import HTTPException, Request, status
+from pydantic import BaseModel
+from src.services.courses.chapters import get_coursechapters_meta
+from src.services.orgs import PublicOrganization
+
+from src.services.users.users import PublicUser
+
+#### Classes ####################################################
+
+
+class ActivityData(BaseModel):
+    activity_id: str
+    activity_type: str
+    data: Optional[dict]
+
+
+class TrailCourse(BaseModel):
+    course_id: str
+    elements_type: Optional[Literal['course']] = 'course'
+    status:  Optional[Literal['ongoing', 'done', 'closed']] = 'ongoing'
+    course_object: dict 
+    masked: Optional[bool] = False
+    activities_marked_complete: Optional[List[str]]
+    activities_data: Optional[List[ActivityData]]
+    progress: Optional[int]
+
+
+class Trail(BaseModel):
+    status:  Optional[Literal['ongoing', 'done', 'closed']] = 'ongoing'
+    masked: Optional[bool] = False
+    courses: Optional[List[TrailCourse]]
+
+
+class TrailInDB(Trail):
+    trail_id: str
+    org_id: str
+    user_id: str
+    creationDate: str = datetime.now().isoformat()
+    updateDate: str = datetime.now().isoformat()
+
+
+#### Classes ####################################################
+
+
+async def create_trail(request: Request, user: PublicUser, org_id: str, trail_object: Trail) -> Trail:
+    trails = request.app.db["trails"]
+
+    # get list of courses
+    if trail_object.courses:
+        courses = trail_object.courses
+        # get course ids
+        course_ids = [course.course_id for course in courses]
+
+        # find if the user has already started the course
+        existing_trail = await trails.find_one({'user_id': user.user_id, 'courses.course_id': {'$in': course_ids}})
+        if existing_trail:
+            # update the status of the element with the matching course_id to "ongoing"
+            for element in existing_trail['courses']:
+                if element['course_id'] in course_ids:
+                    element['status'] = 'ongoing'
+            # update the existing trail in the database
+            await trails.replace_one({'trail_id': existing_trail['trail_id']}, existing_trail)
+
+    # create trail id
+    trail_id = f"trail_{uuid4()}"
+
+    # create trail
+    trail = TrailInDB(**trail_object.dict(), trail_id=trail_id,
+                      user_id=user.user_id, org_id=org_id)
+
+    await trails.insert_one(trail.dict())
+
+    return trail
+
+
+async def get_user_trail(request: Request, org_slug: str, user: PublicUser) -> Trail:
+    trails = request.app.db["trails"]
+    trail = await trails.find_one({"user_id": user.user_id})
+    if not trail:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Trail not found")
+    for element in trail["courses"]:
+        course_id = element["course_id"]
+        chapters_meta = await get_coursechapters_meta(request, course_id, user)
+        activities = chapters_meta["activities"]
+        num_activities = len(activities)
+
+        num_completed_activities = len(
+            element.get("activities_marked_complete", []))
+        element["progress"] = round(
+            (num_completed_activities / num_activities) * 100, 2) if num_activities > 0 else 0
+
+    return Trail(**trail)
+
+
+async def get_user_trail_with_orgslug(request: Request, user: PublicUser, org_slug: str) -> Trail:
+    trails = request.app.db["trails"]
+    orgs = request.app.db["organizations"]
+    courses_mongo = request.app.db["courses"]
+
+    # get org_id from orgslug
+    org = await orgs.find_one({"slug": org_slug})
+
+    trail = await trails.find_one({"user_id": user.user_id, "org_id": org["org_id"]})
+    if not trail:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Trail not found")
+    for courses in trail["courses"]:
+        course_id = courses["course_id"]
+        
+        chapters_meta = await get_coursechapters_meta(request, course_id, user)
+        activities = chapters_meta["activities"]
+        
+        # get course object without _id
+        course_object = await courses_mongo.find_one({"course_id": course_id}, {"_id": 0})
+
+        courses["course_object"] = course_object
+        num_activities = len(activities)
+
+        num_completed_activities = len(
+            courses.get("activities_marked_complete", []))
+        courses["progress"] = round(
+            (num_completed_activities / num_activities) * 100, 2) if num_activities > 0 else 0
+
+    return Trail(**trail)
+
+
+async def add_activity_to_trail(request: Request, user: PublicUser, trail_id: str, course_id: str, activity_id: str) -> Trail:
+    trails = request.app.db["trails"]
+    trail = await trails.find_one({"trail_id": trail_id, "user_id": user.user_id})
+    if not trail:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Trail not found")
+    for element in trail["courses"]:
+        if element["course_id"] == course_id:
+            if activity_id not in element["activities_marked_complete"]:
+                element["activities_marked_complete"].append(activity_id)
+            break
+    await trails.replace_one({"trail_id": trail_id}, trail)
+    return Trail(**trail)
+
+
+async def add_course_to_trail(request: Request, user: PublicUser, orgslug: str, course_id: str) -> Trail:
+    trails = request.app.db["trails"]
+    orgs = request.app.db["organizations"]
+
+
+    org = await orgs.find_one({"slug": orgslug})
+
+    org = PublicOrganization(**org)
+
+    
+
+    trail = await trails.find_one(
+        {"user_id": user.user_id, "org_id": org["org_id"]})
+
+    if not trail:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Trail not found")
+
+    # check if course is already present in the trail
+
+    for element in trail["courses"]:
+        if element["course_id"] == course_id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="Course already present in the trail")
+
+    updated_trail = TrailCourse(course_id=course_id, activities_data=[
+    ], activities_marked_complete=[], progress=0, course_object={}, status="ongoing", masked=False)
+    trail["courses"].append(updated_trail.dict())
+    await trails.replace_one({"trail_id": trail['trail_id']}, trail)
+    return Trail(**trail)
+
+async def remove_course_from_trail(request: Request, user: PublicUser, orgslug: str, course_id: str) -> Trail:
+    trails = request.app.db["trails"]
+    orgs = request.app.db["organizations"]
+
+
+    org = await orgs.find_one({"slug": orgslug})
+
+    org = PublicOrganization(**org)
+
+    print(org)
+
+    trail = await trails.find_one(
+        {"user_id": user.user_id, "org_id": org["org_id"]})
+
+    if not trail:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Trail not found")
+
+    # check if course is already present in the trail
+
+    for element in trail["courses"]:
+        if element["course_id"] == course_id:
+            trail["courses"].remove(element)
+            break
+
+    await trails.replace_one({"trail_id": trail['trail_id']}, trail)
+    return Trail(**trail)
