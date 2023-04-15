427d0cba51cd7624e6aa8b1f85fa5ffcc9360ff6
swve <bdswve@gmail.com>
2023-04-15 22:28:14 +0200

    feat: improve sentry error logging

--- front/sentry.edge.config.js
+++ front/sentry.edge.config.js
@@ -14,4 +14,11 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
+
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  integrations: [new Sentry.Replay()],
 });
