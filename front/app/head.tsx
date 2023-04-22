e9d127bf89c31e68a661ee7f4dfe10b98d19fc60
swve <bdswve@gmail.com>
2023-04-22 11:39:08 +0200

    feat: use client component layout

--- front/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>LearnHouse</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
-    </>
-  )
-}
