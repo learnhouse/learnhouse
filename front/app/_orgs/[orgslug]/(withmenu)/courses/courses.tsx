6fe3f2d591a8c77c289ef90819416f9a08244fa7
swve <bdswve@gmail.com>
2023-05-07 20:03:33 +0000

    feat: show edit button for admins only

--- front/app/_orgs/[orgslug]/(withmenu)/courses/courses.tsx
+++ front/app/_orgs/[orgslug]/(withmenu)/courses/courses.tsx
@@ -9,6 +9,7 @@ import { deleteCourseFromBackend } from '@services/courses/courses';
 import Link from 'next/link';
 import React from 'react'
 import Image from 'next/image';
+import { AuthContext } from '@components/Security/AuthProvider';
 
 interface CourseProps {
     orgslug: string;
@@ -61,16 +62,7 @@ function Courses(props: CourseProps) {
 
                     {courses.map((course: any) => (
                         <div key={course.course_id}>
-                            <div className="flex space-x-2 py-2">
-                                <button className="rounded-md text-sm px-3 font-bold text-red-800 bg-red-200 w-16 flex justify-center items-center" onClick={() => deleteCourses(course.course_id)}>
-                                    Delete <Trash size={10}></Trash>
-                                </button>
-                                <Link href={getUriWithOrg(orgslug, "/course/" + removeCoursePrefix(course.course_id) + "/edit")}>
-                                    <button className="rounded-md text-sm px-3 font-bold text-orange-800 bg-orange-200 w-16 flex justify-center items-center">
-                                        Edit <Edit2 size={10}></Edit2>
-                                    </button>
-                                </Link>
-                            </div>
+                            <AdminEditsArea course={course} course_id={course.course_id} deleteCourses={deleteCourses} />
                             <Link href={getUriWithOrg(orgslug, "/course/" + removeCoursePrefix(course.course_id))}>
                                 <div className="inset-0 ring-1 ring-inset ring-black/10 rounded-lg shadow-xl relative w-[249px] h-[131px] bg-cover" style={{ backgroundImage: `url(${getBackendUrl()}content/uploads/img/${course.thumbnail})` }}>
 
@@ -98,5 +90,66 @@ export const Title = (props: any) => {
         </div>
     )
 }
+
+const AdminEditsArea = (props: any) => {
+    const org_roles_values = ["admin", "owner"];
+    const user_roles_values = ["role_admin"];
+
+    const auth: any = React.useContext(AuthContext);
+    console.log("auth: ", auth);
+
+
+    // this is amazingly terrible code, but gotta release that MVP
+    // TODO: fix this
+    
+    function isAuthorized() {
+        const org_id = props.course.org_id;
+        const org_roles = auth.userInfo.user_object.orgs;
+        const user_roles = auth.userInfo.user_object.roles;
+        const org_role = org_roles.find((org: any) => org.org_id == org_id);
+        const user_role = user_roles.find((role: any) => role.org_id == org_id);
+
+        if (org_role && user_role) {
+            if (org_roles_values.includes(org_role.org_role) && user_roles_values.includes(user_role.role_id)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    // this is amazingly terrible code, but gotta release that MVP
+    // TODO: fix this
+
+    if (auth.isAuthenticated) {
+        if (isAuthorized()) {
+            return (
+                <div className="flex space-x-2 py-2">
+                    <button className="rounded-md text-sm px-3 font-bold text-red-800 bg-red-200 w-16 flex justify-center items-center" onClick={() => props.deleteCourses(props.course_id)}>
+                        Delete <Trash size={10}></Trash>
+                    </button>
+                    <Link href={getUriWithOrg(props.orgslug, "/course/" + props.course_id + "/edit")}>
+                        <button className="rounded-md text-sm px-3 font-bold text-orange-800 bg-orange-200 w-16 flex justify-center items-center">
+                            Edit <Edit2 size={10}></Edit2>
+                        </button>
+                    </Link>
+                </div>
+            )
+        } else {
+            return (
+                <div></div>
+            )
+        }
+    }
+    else {
+        return (
+            <div></div>
+        )
+    }
+}
+
 
 export default Courses
