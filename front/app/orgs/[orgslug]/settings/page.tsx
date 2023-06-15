b9bcb05f3113d139e63bf4714811559fdfe7ef87
swve <bdswve@gmail.com>
2023-06-15 19:39:18 +0200

    feat: migrate org settings to server components

--- front/app/orgs/[orgslug]/settings/page.tsx
+++ front/app/orgs/[orgslug]/settings/page.tsx
@@ -1,4 +1,23 @@
-import React from 'react'
+import { getOrganizationContextInfo } from "@services/organizations/orgs";
+import { Metadata, ResolvingMetadata } from 'next';
+
+type MetadataProps = {
+  params: { orgslug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params }: MetadataProps,
+): Promise<Metadata> {
+
+
+  // Get Org context information 
+  const org = await getOrganizationContextInfo(params.orgslug, { revalidate: 1800, tags: ['organizations'] });
+  return {
+    title: `Settings — ${org.name}`,
+    description: org.description,
+  };
+}
 
 function Settings() {
   return (
