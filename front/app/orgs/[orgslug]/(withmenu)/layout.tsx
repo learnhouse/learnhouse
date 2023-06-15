08023ad5d2a90e4d1d8a3581801e6b070d340018
swve <bdswve@gmail.com>
2023-06-15 16:27:43 +0000

    feat: menu upgrades

--- front/app/orgs/[orgslug]/(withmenu)/layout.tsx
+++ front/app/orgs/[orgslug]/(withmenu)/layout.tsx
@@ -2,7 +2,7 @@ import "@styles/globals.css";
 import { Menu } from "@components/UI/Elements/Menu/Menu";
 import AuthProvider from "@components/Security/AuthProvider";
 
-export default function RootLayout({ children, params }: { children: React.ReactNode , params:any}) {
+export default async function RootLayout({ children, params }: { children: React.ReactNode , params:any}) {
   
   return (
     <>
