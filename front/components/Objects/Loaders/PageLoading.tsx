bfd3f078372cdc5f2c8e8ac1d384ccddb192fa46
swve <bdswve@gmail.com>
2023-06-25 17:30:04 +0200

    feat: redesign trail + add quit course button

--- front/components/Objects/Loaders/PageLoading.tsx
+++ front/components/Objects/Loaders/PageLoading.tsx
@@ -5,8 +5,8 @@ function PageLoading() {
         <div className="max-w-7xl mx-auto px-4 py-20">
             <div className="animate-pulse mx-auto flex space-x-4">
                 <svg className="mx-auto" width="295" height="295" viewBox="0 0 295 295" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <rect opacity="0.51" x="6.5" y="6.5" width="282" height="282" rx="78.5" stroke="#454545" stroke-opacity="0.46" stroke-width="13" stroke-dasharray="11 11" />
-                    <path d="M135.8 200.8V130L122.2 114.6L135.8 110.4V102.8L122.2 87.4L159.8 76V200.8L174.6 218H121L135.8 200.8Z" fill="#454545" fill-opacity="0.13" />
+                    <rect opacity="0.51" x="6.5" y="6.5" width="282" height="282" rx="78.5" stroke="#454545" strokeOpacity="0.46" stroke-width="13" stroke-dasharray="11 11" />
+                    <path d="M135.8 200.8V130L122.2 114.6L135.8 110.4V102.8L122.2 87.4L159.8 76V200.8L174.6 218H121L135.8 200.8Z" fill="#454545" fillOpacity="0.13" />
                 </svg>
             </div>
         </div>
