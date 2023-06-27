c3a5f43f131df2eb018ca569d24c746612794fdc
swve <bdswve@gmail.com>
2023-06-28 00:08:59 +0200

    fix: more various bug and issues

---
+++ src/services/dev/dev.py
@@ -0,0 +1,14 @@
+from fastapi import HTTPException
+from config.config import get_learnhouse_config
+
+
+def isDevModeEnabled():
+    config = get_learnhouse_config()
+    if config.general_config.development_mode:
+        return True
+    else:
+        raise HTTPException(
+            status_code=403,
+            detail="Development mode is not enabled",
+        )
+    
