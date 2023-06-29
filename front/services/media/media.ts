89bbe9868848d10c2239dd342d4fc2a602ea8ec3
swve <bdswve@gmail.com>
2023-06-29 23:42:04 +0200

    feat: implement s3 media upload features

--- front/services/media/media.ts
+++ front/services/media/media.ts
@@ -1,37 +1,46 @@
 import { getBackendUrl } from "@services/config/config";
+const LEARNHOUSE_MEDIA_URL = process.env.NEXT_PUBLIC_LEARNHOUSE_MEDIA_URL;
+
+function getMediaUrl() {
+  if (LEARNHOUSE_MEDIA_URL) {
+    return LEARNHOUSE_MEDIA_URL;
+  } else {
+    return getBackendUrl();
+  }
+}
 
 export function getCourseThumbnailMediaDirectory(orgId: string, courseId: string, fileId: string) {
-  let uri = `${getBackendUrl()}content/${orgId}/courses/${courseId}/thumbnails/${fileId}`;
+  let uri = `${getMediaUrl()}content/${orgId}/courses/${courseId}/thumbnails/${fileId}`;
   return uri;
 }
 
 export function getActivityBlockMediaDirectory(orgId: string, courseId: string, activityId: string, blockId: any, fileId: any, type: string) {
   if (type == "pdfBlock") {
-    let uri = `${getBackendUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/dynamic/blocks/pdfBlock/${blockId}/${fileId}`;
+    let uri = `${getMediaUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/dynamic/blocks/pdfBlock/${blockId}/${fileId}`;
     return uri;
   }
   if (type == "videoBlock") {
-    let uri = `${getBackendUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/dynamic/blocks/videoBlock/${blockId}/${fileId}`;
+    let uri = `${getMediaUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/dynamic/blocks/videoBlock/${blockId}/${fileId}`;
     return uri;
   }
   if (type == "imageBlock") {
-    let uri = `${getBackendUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/dynamic/blocks/imageBlock/${blockId}/${fileId}`;
+    let uri = `${getMediaUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/dynamic/blocks/imageBlock/${blockId}/${fileId}`;
     return uri;
   }
 }
 
 export function getActivityMediaDirectory(orgId: string, courseId: string, activityId: string, fileId: string, activityType: string) {
   if (activityType == "video") {
-    let uri = `${getBackendUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/video/${fileId}`;
+    let uri = `${getMediaUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/video/${fileId}`;
     return uri;
   }
   if (activityType == "documentpdf") {
-    let uri = `${getBackendUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/documentpdf/${fileId}`;
+    let uri = `${getMediaUrl()}content/${orgId}/courses/${courseId}/activities/${activityId}/documentpdf/${fileId}`;
     return uri;
   }
 }
 
 export function getOrgLogoMediaDirectory(orgId: string, fileId: string) {
-  let uri = `${getBackendUrl()}content/${orgId}/logos/${fileId}`;
+  let uri = `${getMediaUrl()}content/${orgId}/logos/${fileId}`;
   return uri;
 }
