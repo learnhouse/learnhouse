60a906a199f95cbda22f3cae87434a6bc56699ba
swve <bdswve@gmail.com>
2023-07-12 15:19:46 +0100

    fix: linting errors

--- src/core/events/content.py
+++ src/core/events/content.py
@@ -2,7 +2,7 @@ import os
 
 
 async def check_content_directory():
-    if not os.path.exists(f"content"):
+    if not os.path.exists("content"):
         # create folder for activity
         print("Creating content directory...")
-        os.makedirs(f"content")
+        os.makedirs("content")
