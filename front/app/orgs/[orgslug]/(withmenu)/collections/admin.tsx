30f7a9ac07ccd6477132d82fc5c5b551c7a099fc
swve <bdswve@gmail.com>
2023-07-25 20:39:09 +0200

    feat: add delete confirmation screens

--- front/app/orgs/[orgslug]/(withmenu)/collections/admin.tsx
+++ front/app/orgs/[orgslug]/(withmenu)/collections/admin.tsx
@@ -2,6 +2,7 @@
 
 import AuthenticatedClientElement from '@components/Security/AuthenticatedClientElement';
 import { AuthContext } from '@components/Security/AuthProvider';
+import ConfirmationModal from '@components/StyledElements/ConfirmationModal/ConfirmationModal';
 import { getUriWithOrg } from '@services/config/config';
 import { deleteCollection } from '@services/courses/collections';
 import { revalidateTags } from '@services/utils/ts/requests';
@@ -26,9 +27,17 @@ const CollectionAdminEditsArea = (props: any) => {
     return (
         <AuthenticatedClientElement orgId={props.org_id} checkMethod='roles'>
             <div className="flex space-x-2 py-2">
-                <button className="rounded-md text-sm px-3 font-bold text-red-800 bg-red-200 w-16 flex justify-center items-center" onClick={() => deleteCollectionUI(props.collection_id)}>
-                    Delete <Trash size={10}></Trash>
-                </button>
+                <ConfirmationModal
+                    confirmationMessage="Are you sure you want to delete this collection?"
+                    confirmationButtonText="Delete Collection"
+                    dialogTitle={"Delete " + props.collection.name + " ?"}
+                    dialogTrigger={
+                        <button className="rounded-md text-sm px-3 font-bold text-red-800 bg-red-200 w-16 flex justify-center items-center" >
+                            Delete <Trash size={10}></Trash>
+                        </button>}
+                    functionToExecute={() => deleteCollectionUI(props.collection_id)}
+                    status='warning'
+                ></ConfirmationModal>
             </div>
         </AuthenticatedClientElement>
     )
