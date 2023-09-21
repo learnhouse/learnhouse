d0a3b818ac176433375915942cadea1204d821cc
swve <bdswve@gmail.com>
2023-09-21 17:55:28 +0200

    feat: refactor + home improvements

--- front/app/orgs/[orgslug]/(withmenu)/page.tsx
+++ front/app/orgs/[orgslug]/(withmenu)/page.tsx
@@ -1,20 +1,20 @@
 export const dynamic = 'force-dynamic';
-import { Metadata, ResolvingMetadata } from 'next';
-import { getBackendUrl, getUriWithOrg } from "@services/config/config";
-import { getCourse, getOrgCourses, getOrgCoursesWithAuthHeader } from "@services/courses/courses";
-
+import { Metadata } from 'next';
+import { getUriWithOrg } from "@services/config/config";
+import { getOrgCoursesWithAuthHeader } from "@services/courses/courses";
 import Link from "next/link";
-import Image from "next/image";
-import { getOrgCollections, getOrgCollectionsWithAuthHeader } from "@services/courses/collections";
+import { getOrgCollectionsWithAuthHeader } from "@services/courses/collections";
 import { getOrganizationContextInfo } from '@services/organizations/orgs';
-
 import { cookies } from 'next/headers';
 import GeneralWrapperStyled from '@components/StyledElements/Wrappers/GeneralWrapper';
 import TypeOfContentTitle from '@components/StyledElements/Titles/TypeOfContentTitle';
-import { getCourseThumbnailMediaDirectory } from '@services/media/media';
-import { getAccessTokenFromRefreshTokenCookie, getNewAccessTokenUsingRefreshTokenServer } from '@services/auth/auth';
+import { getAccessTokenFromRefreshTokenCookie } from '@services/auth/auth';
 import CourseThumbnail from '@components/Objects/Other/CourseThumbnail';
 import CollectionThumbnail from '@components/Objects/Other/CollectionThumbnail';
+import AuthenticatedClientElement from '@components/Security/AuthenticatedClientElement';
+import { Plus, PlusCircle } from 'lucide-react';
+import NewCourseButton from '@components/StyledElements/Buttons/NewCourseButton';
+import NewCollectionButton from '@components/StyledElements/Buttons/NewCollectionButton';
 
 type MetadataProps = {
   params: { orgslug: string };
@@ -56,40 +56,85 @@ const OrgHomePage = async (params: any) => {
   const access_token = await getAccessTokenFromRefreshTokenCookie(cookieStore)
   const courses = await getOrgCoursesWithAuthHeader(orgslug, { revalidate: 0, tags: ['courses'] }, access_token ? access_token : null);
   const org = await getOrganizationContextInfo(orgslug, { revalidate: 1800, tags: ['organizations'] });
+  const org_id = org.org_id;
   const collections = await getOrgCollectionsWithAuthHeader(org.org_id, access_token ? access_token : null, { revalidate: 0, tags: ['courses'] });
-
-
-  // function to remove "course_" from the course_id
-  function removeCoursePrefix(course_id: string) {
-    return course_id.replace("course_", "");
-  }
-
-  function removeCollectionPrefix(collection_id: string) {
-    return collection_id.replace("collection_", "");
-  }
 
   return (
     <div>
       <GeneralWrapperStyled>
         {/* Collections */}
-        <TypeOfContentTitle title="Collections" type="col" />
+        <div className='flex items-center '>
+          <div className='flex grow'>
+            <TypeOfContentTitle title="Collections" type="col" />
+          </div>
+          <AuthenticatedClientElement checkMethod='roles' orgId={org_id}>
+            <Link href={getUriWithOrg(orgslug, "/collections/new")}>
+              <NewCollectionButton />
+            </Link>
+          </AuthenticatedClientElement>
+        </div>
         <div className="home_collections flex flex-wrap">
           {collections.map((collection: any) => (
             <div className="flex flex-col py-3 px-3" key={collection.collection_id}>
               <CollectionThumbnail collection={collection} orgslug={orgslug} org_id={org.org_id} />
             </div>
           ))}
+          {collections.length == 0 &&
+            <div className="flex mx-auto h-[100px]">
+              <div className="flex flex-col justify-center text-center items-center space-y-3">
+                <div className="flex flex-col space-y-3">
+                  <div className='mx-auto'>
+                    <svg width="50" height="50" viewBox="0 0 295 295" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <rect opacity="0.51" x="10" y="10" width="275" height="275" rx="75" stroke="#4B5564" stroke-opacity="0.15" stroke-width="20" />
+                      <path d="M135.8 200.8V130L122.2 114.6L135.8 110.4V102.8L122.2 87.4L159.8 76V200.8L174.6 218H121L135.8 200.8Z" fill="#4B5564" fill-opacity="0.08" />
+                    </svg>
+                  </div>
+                  <div className="space-y-0">
+                    <h1 className="text-xl font-bold text-gray-600">No collections yet</h1>
+                    <p className="text-md text-gray-400">Create a collection to group courses together</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
         </div>
 
         {/* Courses */}
         <div className='h-5'></div>
-        <TypeOfContentTitle title="Courses" type="cou" />
+        <div className='flex items-center '>
+          <div className='flex grow'>
+            <TypeOfContentTitle title="Courses" type="cou" />
+          </div>
+          <AuthenticatedClientElement checkMethod='roles' orgId={org_id}>
+            <Link href={getUriWithOrg(orgslug, "/courses?new=true")}>
+              <NewCourseButton />
+            </Link>
+          </AuthenticatedClientElement>
+        </div>
         <div className="home_courses flex flex-wrap">
           {courses.map((course: any) => (
             <div className="py-3 px-3" key={course.course_id}>
               <CourseThumbnail course={course} orgslug={orgslug} />
             </div>
           ))}
+          {courses.length == 0 &&
+            <div className="flex mx-auto h-[300px]">
+              <div className="flex flex-col justify-center text-center items-center space-y-3">
+                <div className="flex flex-col space-y-3">
+                  <div className='mx-auto'>
+                    <svg width="50" height="50" viewBox="0 0 295 295" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <rect opacity="0.51" x="10" y="10" width="275" height="275" rx="75" stroke="#4B5564" stroke-opacity="0.15" stroke-width="20" />
+                      <path d="M135.8 200.8V130L122.2 114.6L135.8 110.4V102.8L122.2 87.4L159.8 76V200.8L174.6 218H121L135.8 200.8Z" fill="#4B5564" fill-opacity="0.08" />
+                    </svg>
+                  </div>
+                  <div className="space-y-0">
+                    <h1 className="text-xl font-bold text-gray-600">No courses yet</h1>
+                    <p className="text-md text-gray-400">Create a course to add content</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
         </div>
       </GeneralWrapperStyled>
     </div>
--- front/components/Objects/Other/CollectionThumbnail.tsx
+++ front/components/Objects/Other/CollectionThumbnail.tsx
@@ -22,23 +22,23 @@ const removeCollectionPrefix = (collectionid: string) => {
 
 function CollectionThumbnail(props: PropsType) {
     return (
-        <div className='relative'>
-            <CollectionAdminEditsArea orgslug={props.orgslug} org_id={props.org_id} collection_id={props.collection.collection_id} collection={props.collection} />
-            <Link href={getUriWithOrg(props.orgslug, "/collection/" + removeCollectionPrefix(props.collection.collection_id))}>
-                <div className="space-y-2 inset-0 ring-1 ring-inset my-auto ring-black/10 rounded-xl shadow-xl relative w-[249px] h-[180px] bg-cover flex flex-col items-center justify-center bg-indigo-600 font-bold text-zinc-50" >
-                    <div className="flex -space-x-5">
-                        {props.collection.courses.slice(0, 3).map((course: any) => (
-                            <Link key={course.course_id} href={getUriWithOrg(props.orgslug, "/course/" + course.course_id.substring(7))}>
-
-                                <div className="inset-0 rounded-xl shadow-xl bg-cover w-24 h-12 justify-center ring-indigo-800 ring-4" style={{ backgroundImage: `url(${getCourseThumbnailMediaDirectory(props.collection.org_id, course.course_id, course.thumbnail)})` }}>
-
+        <div className=''>
+            <div className="flex flex-row space-x-4 inset-0 ring-1 ring-inset my-auto ring-black/10 rounded-xl shadow-xl relative w-[300px] h-[80px] bg-cover items-center justify-center bg-indigo-600 font-bold text-zinc-50" >
+                <div className="flex -space-x-5">
+                    {props.collection.courses.slice(0, 2).map((course: any) => (
+                        <>
+                            <Link href={getUriWithOrg(props.orgslug, "/collection/" + removeCollectionPrefix(props.collection.collection_id))}>
+                                <div className="inset-0 rounded-full shadow-2xl bg-cover w-12 h-8 justify-center ring-indigo-800 ring-4" style={{ backgroundImage: `url(${getCourseThumbnailMediaDirectory(props.collection.org_id, course.course_id, course.thumbnail)})` }}>
                                 </div>
                             </Link>
-                        ))}
-                    </div>
-                    <h1 className="font-bold text-lg justify-center">{props.collection.name}</h1>
+                        </>
+                    ))}
                 </div>
-            </Link>
+                <Link href={getUriWithOrg(props.orgslug, "/collection/" + removeCollectionPrefix(props.collection.collection_id))}>
+                    <h1 className="font-bold text-md justify-center">{props.collection.name}</h1>
+                </Link>
+                <CollectionAdminEditsArea orgslug={props.orgslug} org_id={props.org_id} collection_id={props.collection.collection_id} collection={props.collection} />
+            </div>
         </div>
     )
 }
@@ -51,24 +51,20 @@ const CollectionAdminEditsArea = (props: any) => {
         await revalidateTags(["collections"], props.orgslug);
         // reload the page
         router.refresh();
-        router.push(getUriWithOrg(props.orgslug, "/collections"));
-
-        // refresh page (FIX for Next.js BUG)
-        //window.location.reload();
     }
 
     return (
         <AuthenticatedClientElement orgId={props.org_id} checkMethod='roles'>
-            <div className="flex space-x-1 absolute justify-center mx-auto z-20 bottom-4 left-1/2 transform -translate-x-1/2">
+            <div className="flex space-x-1  justify-center mx-auto z-20 ">
                 <ConfirmationModal
                     confirmationMessage="Are you sure you want to delete this collection?"
                     confirmationButtonText="Delete Collection"
                     dialogTitle={"Delete " + props.collection.name + " ?"}
                     dialogTrigger={
                         <div
-                            className="hover:cursor-pointer p-1 px-4 bg-red-600 rounded-xl items-center justify-center flex shadow-lg"
+                            className="hover:cursor-pointer p-1 px-2 bg-red-600 rounded-xl items-center justify-center flex shadow-xl"
                             rel="noopener noreferrer">
-                            <X size={15} className="text-rose-200 font-bold" />
+                            <X size={10} className="text-rose-200 font-bold" />
                         </div>}
                     functionToExecute={() => deleteCollectionUI(props.collection_id)}
                     status='warning'
