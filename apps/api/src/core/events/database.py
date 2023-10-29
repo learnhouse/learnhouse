8708d4dfbbe7840be7a34be1e48b96a4a77bf67a
swve <bdswve@gmail.com>
2023-10-29 13:46:02 +0100

    wip: add more schemas

--- apps/api/src/core/events/database.py
+++ apps/api/src/core/events/database.py
@@ -2,26 +2,46 @@ import logging
 from fastapi import FastAPI
 import motor.motor_asyncio
 from sqlmodel import Field, SQLModel, Session, create_engine
-from src.rewrite.services.db import users
 
-engine = create_engine('postgresql://learnhouse:learnhouse@db:5432/learnhouse', echo=True)
+from src.rewrite.services.db import (
+    user_organizations,
+    users,
+    roles,
+    organization_settings,
+    organizations,
+    courses,
+    course_authors,
+    chapters,
+    activities,
+    course_chapters,
+    chapter_activities,
+    blocks,
+    collections,
+)
+
+engine = create_engine(
+    "postgresql://learnhouse:learnhouse@db:5432/learnhouse", echo=True
+)
 SQLModel.metadata.create_all(engine)
+
 
 async def connect_to_db(app: FastAPI):
     app.db_engine = engine  # type: ignore
     logging.info("LearnHouse database has been started.")
 
-    
     SQLModel.metadata.create_all(engine)
 
-    # mongodb 
+    # mongodb
     app.mongodb_client = motor.motor_asyncio.AsyncIOMotorClient(  # type: ignore
-            app.learnhouse_config.database_config.mongodb_connection_string)  # type: ignore
-    app.db = app.mongodb_client["learnhouse"] # type: ignore
+        app.learnhouse_config.database_config.mongodb_connection_string  # type: ignore
+    )  # type: ignore
+    app.db = app.mongodb_client["learnhouse"]  # type: ignore
+
 
 def get_db_session():
     with Session(engine) as session:
         yield session
+
 
 async def close_database(app: FastAPI):
     app.mongodb_client.close()  # type: ignore
