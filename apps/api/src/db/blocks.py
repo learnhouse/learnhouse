b04fd64c92b64369e4fe277b80186fd6087044cc
swve <bdswve@gmail.com>
2023-11-16 22:37:09 +0100

    feat: inot blocks + schemas updates

--- apps/api/src/db/blocks.py
+++ apps/api/src/db/blocks.py
@@ -5,16 +5,16 @@ from enum import Enum
 
 
 class BlockTypeEnum(str, Enum):
-    QUIZ_BLOCK = "QUIZ_BLOCK"
-    VIDEO_BLOCK = "VIDEO_BLOCK"
-    DOCUMENT_PDF_BLOCK = "DOCUMENT_PDF_BLOCK"
-    IMAGE_BLOCK = "IMAGE_BLOCK"
-    CUSTOM = "CUSTOM"
+    BLOCK_QUIZ = "BLOCK_QUIZ"
+    BLOCK_VIDEO = "BLOCK_VIDEO"
+    BLOCK_DOCUMENT_PDF = "BLOCK_DOCUMENT_PDF"
+    BLOCK_IMAGE = "BLOCK_IMAGE"
+    BLOCK_CUSTOM = "BLOCK_CUSTOM"
 
 
 class BlockBase(SQLModel):
     id: Optional[int] = Field(default=None, primary_key=True)
-    block_type: BlockTypeEnum = BlockTypeEnum.CUSTOM
+    block_type: BlockTypeEnum = BlockTypeEnum.BLOCK_CUSTOM
     content: dict = Field(default={}, sa_column=Column(JSON))
 
 
@@ -29,8 +29,10 @@ class Block(BlockBase, table=True):
     creation_date: str
     update_date: str
 
+
 class BlockCreate(BlockBase):
     pass
+
 
 class BlockRead(BlockBase):
     id: int
