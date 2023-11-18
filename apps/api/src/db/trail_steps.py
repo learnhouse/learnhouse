89b6a77eecbfd3d2b47dc2dedcfe38bf557c1776
swve <bdswve@gmail.com>
2023-11-18 12:22:00 +0100

    feat: init trails

---
+++ apps/api/src/db/trail_steps.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from typing import Optional
+from sqlalchemy import JSON, Column
+from sqlmodel import Field, SQLModel
+
+
+class TrailStepTypeEnum(str, Enum):
+    STEP_TYPE_READABLE_ACTIVITY = "STEP_TYPE_READABLE_ACTIVITY"
+    STEP_TYPE_ASSIGNMENT_ACTIVITY = "STEP_TYPE_ASSIGNMENT_ACTIVITY"
+    STEP_TYPE_CUSTOM_ACTIVITY = "STEP_TYPE_CUSTOM_ACTIVITY"
+
+
+class TrailStep(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    complete: bool
+    teacher_verified: bool
+    grade: str
+    data: dict = Field(default={}, sa_column=Column(JSON))
+    # foreign keys
+    trailrun_id: int = Field(default=None, foreign_key="trailrun.id")
+    trail_id: int = Field(default=None, foreign_key="trail.id")
+    activity_id: int = Field(default=None, foreign_key="activity.id")
+    course_id: int = Field(default=None, foreign_key="course.id")
+    org_id: int = Field(default=None, foreign_key="organization.id")
+    user_id: int = Field(default=None, foreign_key="user.id")
+    # timestamps
+    creation_date: str
+    update_date: str
+
+
+# note : prepare assignments support
+# an assignment object will be linked to a trail step object in the future
