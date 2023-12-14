ac70cb534d0c72465879349b76b625cb1746bd63
swve <bdswve@gmail.com>
2023-12-14 21:24:50 +0100

    fix: remove mongo

--- apps/api/src/core/events/database.py
+++ apps/api/src/core/events/database.py
@@ -1,11 +1,11 @@
 import logging
+from config.config import get_learnhouse_config
 from fastapi import FastAPI
-import motor.motor_asyncio
 from sqlmodel import SQLModel, Session, create_engine
 
-
+learnhouse_config = get_learnhouse_config()
 engine = create_engine(
-    "postgresql://learnhouse:learnhouse@db:5432/learnhouse", echo=False
+    learnhouse_config.database_config.sql_connection_string, echo=False
 )
 SQLModel.metadata.create_all(engine)
 
@@ -16,12 +16,6 @@ async def connect_to_db(app: FastAPI):
 
     SQLModel.metadata.create_all(engine)
 
-    # mongodb
-    app.mongodb_client = motor.motor_asyncio.AsyncIOMotorClient(  # type: ignore
-        app.learnhouse_config.database_config.mongodb_connection_string  # type: ignore
-    )  # type: ignore
-    app.db = app.mongodb_client["learnhouse"]  # type: ignore
-
 
 def get_db_session():
     with Session(engine) as session:
@@ -29,6 +23,5 @@ def get_db_session():
 
 
 async def close_database(app: FastAPI):
-    app.mongodb_client.close()  # type: ignore
     logging.info("LearnHouse has been shut down.")
     return app
