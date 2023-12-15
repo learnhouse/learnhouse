d9c585962a4b1d5f08116cc8655266355dd4a87f
swve <bdswve@gmail.com>
2023-12-15 21:37:22 +0100

    feat: init migration script from mongodb

--- apps/api/src/core/events/database.py
+++ apps/api/src/core/events/database.py
@@ -2,10 +2,11 @@ import logging
 from config.config import get_learnhouse_config
 from fastapi import FastAPI
 from sqlmodel import SQLModel, Session, create_engine
+import motor.motor_asyncio
 
 learnhouse_config = get_learnhouse_config()
 engine = create_engine(
-    learnhouse_config.database_config.sql_connection_string, echo=False
+    learnhouse_config.database_config.sql_connection_string, echo=False  # type: ignore
 )
 SQLModel.metadata.create_all(engine)
 
@@ -13,8 +14,14 @@ SQLModel.metadata.create_all(engine)
 async def connect_to_db(app: FastAPI):
     app.db_engine = engine  # type: ignore
     logging.info("LearnHouse database has been started.")
-
     SQLModel.metadata.create_all(engine)
+
+    # MongoDB for migration purposes
+    # mongodb
+    app.mongodb_client = motor.motor_asyncio.AsyncIOMotorClient(  # type: ignore
+        app.learnhouse_config.database_config.mongo_connection_string  # type: ignore
+    )  # type: ignore
+    app.db = app.mongodb_client["learnhouse"]  # type: ignore
 
 
 def get_db_session():
--- apps/api/src/routers/dev.py
+++ apps/api/src/routers/dev.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, Request
+from sqlmodel import Session
+from src.core.events.database import get_db_session
+from src.services.dev.migration_from_mongo import start_migrate_from_mongo
 from config.config import get_learnhouse_config
 
 
@@ -9,3 +12,11 @@ router = APIRouter()
 async def config():
     config = get_learnhouse_config()
     return config.dict()
+
+
+@router.get("/migrate_from_mongo")
+async def migrate_from_mongo(
+    request: Request,
+    db_session: Session = Depends(get_db_session),
+):
+    return await start_migrate_from_mongo(request, db_session)
