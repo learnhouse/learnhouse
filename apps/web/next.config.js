98d2d7cf8311190eeebf839fc76bcd4d184400e0
swve <bdswve@gmail.com>
2024-01-04 23:40:31 +0100

    fix: reconfigure sentry for the frontend

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,4 +3,23 @@ const nextConfig = {
     reactStrictMode: false,
 }
 
-module.exports = nextConfig
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const SentryWebpackPluginOptions = {
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
+
+  // Transpiles SDK to be compatible with IE11 (increases bundle size)
+  transpileClientSDK: true,
+
+  // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+  tunnelRoute: "/monitoring",
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+}
+
+module.exports = withSentryConfig(nextConfig, SentryWebpackPluginOptions);
