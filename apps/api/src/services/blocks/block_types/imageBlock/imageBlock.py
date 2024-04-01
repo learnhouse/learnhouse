8104a444b71ad167241506ba3341f7539a8de14a
swve <bdswve@gmail.com>
2024-04-01 16:26:17 +0200

    fix: block upload bug

--- apps/api/src/services/blocks/block_types/imageBlock/imageBlock.py
+++ apps/api/src/services/blocks/block_types/imageBlock/imageBlock.py
@@ -54,7 +54,7 @@ async def create_image_block(
     block = Block(
         activity_id=activity.id if activity.id else 0,
         block_type=BlockTypeEnum.BLOCK_IMAGE,
-        content=block_data.model_dump(),
+        content=block_data.dict(),
         org_id=org.id if org.id else 0,
         course_id=course.id if course.id else 0,
         block_uuid=block_uuid,
@@ -80,7 +80,7 @@ async def get_image_block(
 
     if block:
 
-        block = BlockRead.model_validate(block)
+        block = BlockRead.from_orm(block)
         
         return block
     else:
