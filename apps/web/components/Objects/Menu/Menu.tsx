f0e5fa925d71363ad7202742e054302c199a62a0
swve <bdswve@gmail.com>
2024-04-16 21:54:55 +0200

    feat: add ui skeletons for course updates

--- apps/web/components/Objects/Menu/Menu.tsx
+++ apps/web/components/Objects/Menu/Menu.tsx
@@ -23,14 +23,8 @@ export const Menu = (props: any) => {
 
   return (
     <>
-      <div className="backdrop-blur-lg h-[60px] blur-3xl z-10" style={{}}>
-        <div
-          className="h-[150px] blur-3xl z-0"
-          style={{
-            background:
-              'radial-gradient(1397.20% 56.18% at 75.99% 53.73%, rgba(253, 182, 207, 0.08) 0%, rgba(3, 110, 146, 0.08) 100%)',
-          }}
-        ></div>
+      <div className="backdrop-blur-lg h-[60px] blur-3xl -z-10" style={{}}>
+        
       </div>
       <div className="backdrop-blur-lg bg-white/90 fixed flex top-0 left-0 right-0 h-[60px] ring-1 ring-inset ring-gray-500/10 items-center space-x-5 shadow-[0px_4px_16px_rgba(0,0,0,0.03)] z-50">
         <div className="flex items-center space-x-5 w-full max-w-screen-2xl mx-auto px-16">
