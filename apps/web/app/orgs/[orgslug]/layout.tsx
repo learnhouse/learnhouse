b96a996951ebab0b8d3aaecad1459a8e46304641
swve <bdswve@gmail.com>
2024-06-13 23:00:33 +0100

    feat: add loading indicator and improve loading screens

--- apps/web/app/orgs/[orgslug]/layout.tsx
+++ apps/web/app/orgs/[orgslug]/layout.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { OrgProvider } from '@components/Contexts/OrgContext'
+import NextTopLoader from 'nextjs-toploader';
 import Toast from '@components/StyledElements/Toast/Toast'
 import '@styles/globals.css'
 
@@ -13,6 +14,7 @@ export default function RootLayout({
   return (
     <div>
       <OrgProvider orgslug={params.orgslug}>
+        <NextTopLoader color="#2e2e2e" initialPosition={0.3} height={4}  easing={'ease'} speed={500} showSpinner={false} />
         <Toast />
         {children}
       </OrgProvider>
