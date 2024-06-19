79da5b8e87e52d07b6795daa9a78a02cdb1fa15e
swve <bdswve@gmail.com>
2024-06-19 15:43:38 +0100

    feat: init onboarding

--- apps/web/app/orgs/[orgslug]/layout.tsx
+++ apps/web/app/orgs/[orgslug]/layout.tsx
@@ -3,6 +3,7 @@ import { OrgProvider } from '@components/Contexts/OrgContext'
 import NextTopLoader from 'nextjs-toploader';
 import Toast from '@components/StyledElements/Toast/Toast'
 import '@styles/globals.css'
+import Onboarding from '@components/Onboarding/Onboarding';
 
 export default function RootLayout({
   children,
@@ -16,6 +17,7 @@ export default function RootLayout({
       <OrgProvider orgslug={params.orgslug}>
         <NextTopLoader color="#2e2e2e" initialPosition={0.3} height={4}  easing={'ease'} speed={500} showSpinner={false} />
         <Toast />
+        <Onboarding />
         {children}
       </OrgProvider>
     </div>
