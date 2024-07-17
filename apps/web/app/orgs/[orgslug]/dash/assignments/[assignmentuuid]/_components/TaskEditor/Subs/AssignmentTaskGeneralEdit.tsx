ac47ce43df52dad4453f4f8d498099fa4b2761a8
swve <bdswve@gmail.com>
2024-07-17 23:41:46 +0200

    feat: full quiz tasks edition and creation

--- apps/web/app/orgs/[orgslug]/dash/assignments/[assignmentuuid]/_components/TaskEditor/Subs/AssignmentTaskGeneralEdit.tsx
+++ apps/web/app/orgs/[orgslug]/dash/assignments/[assignmentuuid]/_components/TaskEditor/Subs/AssignmentTaskGeneralEdit.tsx
@@ -16,103 +16,7 @@ import React, { use, useEffect } from 'react'
 import toast from 'react-hot-toast';
 import { mutate } from 'swr';
 
-function AssignmentTaskEditor({ page }: any) {
-    const [selectedSubPage, setSelectedSubPage] = React.useState(page)
-    const assignment = useAssignments() as any
-    const assignmentTaskState = useAssignmentsTask() as any
-    const assignmentTaskStateHook = useAssignmentsTaskDispatch() as any
-    const session = useLHSession() as any;
-    const access_token = session?.data?.tokens?.access_token;
-
-    async function deleteTaskUI() {
-        const res = await deleteAssignmentTask(assignmentTaskState.assignmentTask.assignment_task_uuid, assignment.assignment_object.assignment_uuid, access_token)
-        if (res) {
-            assignmentTaskStateHook({
-                type: 'SET_MULTIPLE_STATES',
-                payload: {
-                  selectedAssignmentTaskUUID: null,
-                  assignmentTask: {},
-                },
-              });
-              mutate(`${getAPIUrl()}assignments/${assignment.assignment_object.assignment_uuid}/tasks`)
-            toast.success('Task deleted successfully')
-        } else {
-            toast.error('Error deleting task, please retry later.')
-        }
-    }
-
-    useEffect(() => {
-    }
-        , [assignmentTaskState,assignmentTaskStateHook])
-
-    return (
-        <div className="flex flex-col font-black text-sm w-full z-20">
-            {assignmentTaskState.assignmentTask && Object.keys(assignmentTaskState.assignmentTask).length > 0 && (
-                <div className='flex flex-col space-y-3'>
-                    <div className='flex flex-col bg-white pl-10 pr-10 text-sm tracking-tight  z-10 shadow-[0px_4px_16px_rgba(0,0,0,0.06)] pt-5 mb-3 nice-shadow'>
-                        <div className='flex py-1 justify-between items-center'>
-                            <div className='font-semibold text-lg '>
-                                {assignmentTaskState?.assignmentTask.title}
-                            </div>
-                            <div>
-                                <div
-                                    onClick={() => deleteTaskUI()}
-                                    className='flex px-2 py-1.5 cursor-pointer rounded-md space-x-2 items-center bg-gradient-to-bl text-red-800  bg-rose-100  border border-rose-600/10 shadow-rose-900/10 shadow-lg'>
-                                    <Trash size={18} />
-                                    <p className='text-xs font-semibold'>Delete Task</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='flex space-x-2 '>
-                            <div
-                                onClick={() => setSelectedSubPage('general')}
-                                className={`flex space-x-4 py-2 w-fit text-center border-black transition-all ease-linear ${selectedSubPage === 'general'
-                                    ? 'border-b-4'
-                                    : 'opacity-50'
-                                    } cursor-pointer`}
-                            >
-                                <div className="flex items-center space-x-2.5 mx-2">
-                                    <Info size={16} />
-                                    <div>General</div>
-                                </div>
-                            </div>
-                            <div
-                                onClick={() => setSelectedSubPage('content')}
-                                className={`flex space-x-4 py-2 w-fit text-center border-black transition-all ease-linear ${selectedSubPage === 'content'
-                                    ? 'border-b-4'
-                                    : 'opacity-50'
-                                    } cursor-pointer`}
-                            >
-                                <div className="flex items-center space-x-2.5 mx-2">
-                                    <GalleryVerticalEnd size={16} />
-                                    <div>Content</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='ml-10 mr-10 mt-10 mx-auto bg-white rounded-xl shadow-sm px-6 py-5 nice-shadow'>
-                        {selectedSubPage === 'general' && <AssignmentTaskGeneralEdit />}
-                    </div>
-                </div>
-            )}
-            {Object.keys(assignmentTaskState.assignmentTask).length == 0 && (
-                <div className='flex flex-col h-full bg-white pl-10 pr-10 text-sm tracking-tight  z-10 shadow-[0px_4px_16px_rgba(0,0,0,0.06)] pt-5'>
-                    <div className='flex justify-center items-center h-full text-gray-300 antialiased'>
-                        <div className='flex flex-col space-y-2 items-center'>
-                            <TentTree size={60} />
-                            <div className='font-semibold text-2xl py-1'>
-                                No Task Selected
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            )}
-
-        </div>
-    )
-}
-
-function AssignmentTaskGeneralEdit() {
+export function AssignmentTaskGeneralEdit() {
     const session = useLHSession() as any;
     const access_token = session?.data?.tokens?.access_token;
     const assignmentTaskState = useAssignmentsTask() as any
@@ -126,8 +30,6 @@ function AssignmentTaskGeneralEdit() {
         }
         return errors;
     };
-
-
 
     const formik = useFormik({
         initialValues: {
@@ -374,5 +276,3 @@ function UpdateTaskRef() {
         </div>
     )
 }
-
-export default AssignmentTaskEditor
--- apps/web/app/orgs/[orgslug]/dash/assignments/[assignmentuuid]/page.tsx
+++ apps/web/app/orgs/[orgslug]/dash/assignments/[assignmentuuid]/page.tsx
@@ -1,56 +1,57 @@
 'use client';
 import BreadCrumbs from '@components/Dashboard/UI/BreadCrumbs'
 import { BookOpen, BookX, EllipsisVertical, LayoutList } from 'lucide-react'
-import React from 'react'
-import AssignmentTaskEditor from './_components/TaskEditor';
-import { AssignmentProvider } from '@components/Contexts/Assignments/AssignmentContext';
+import React, { useEffect } from 'react'
+import { AssignmentProvider, useAssignments } from '@components/Contexts/Assignments/AssignmentContext';
 import AssignmentTasks from './_components/Tasks';
 import { useParams } from 'next/navigation';
 import { AssignmentsTaskProvider } from '@components/Contexts/Assignments/AssignmentsTaskContext';
 import ToolTip from '@components/StyledElements/Tooltip/Tooltip';
+import AssignmentTaskEditor from './_components/TaskEditor/TaskEditor';
 
 function AssignmentEdit() {
     const params = useParams<{ assignmentuuid: string; }>()
     return (
         <div className='flex w-full flex-col'>
-            <div className='pb-5  bg-white z-50 shadow-[0px_4px_16px_rgba(0,0,0,0.06)] nice-shadow'>
-                <div className='flex justify-between mr-10 h-full'>
-                    <div className="pl-10 mr-10 tracking-tighter">
-                        <BreadCrumbs type="assignments" last_breadcrumb='UUID' />
-                        <div className="w-100 flex justify-between">
-                            <div className="flex font-bold text-2xl">Assignment Editor</div>
+            <AssignmentProvider assignment_uuid={'assignment_' + params.assignmentuuid}>
+                <div className='pb-5  bg-white z-50 shadow-[0px_4px_16px_rgba(0,0,0,0.06)] nice-shadow'>
+                    <div className='flex justify-between mr-10 h-full'>
+                        <div className="pl-10 mr-10 tracking-tighter">
+                        <BrdCmpx />
+                            <div className="w-100 flex justify-between">
+                                <div className="flex font-bold text-2xl">Assignment Editor</div>
+                            </div>
                         </div>
-                    </div>
-                    <div className='flex flex-col justify-center antialiased'>
-                        <div className='flex mx-auto mt-5 items-center space-x-4'>
-                            <div className='flex bg-green-200/60 text-xs rounded-full px-3.5 py-2 mx-auto font-bold outline outline-1 outline-green-300'>Published</div>
-                            <div><EllipsisVertical className='text-gray-500' size={13} /></div>
-                            <ToolTip
-                                side='left'
-                                slateBlack
-                                sideOffset={10}
-                                content="Make your Assignment public and available for students" >
-                                <div className='flex px-3 py-2 cursor-pointer rounded-md space-x-2 items-center bg-gradient-to-bl text-green-800 font-medium from-green-400/50 to-lime-200/80  border border-green-600/10 shadow-green-900/10 shadow-lg'>
-                                    <BookOpen size={18} />
-                                    <p className=' text-sm font-bold'>Publish</p>
-                                </div>
-                            </ToolTip>
-                            <ToolTip
-                                side='left'
-                                slateBlack
-                                sideOffset={10}
-                                content="Make your Assignment unavailable for students" >
-                                <div className='flex px-3 py-2 cursor-pointer rounded-md space-x-2 items-center bg-gradient-to-bl text-gray-800 font-medium from-gray-400/50 to-gray-200/80 border border-gray-600/10 shadow-gray-900/10 shadow-lg'>
-                                    <BookX size={18} />
-                                    <p className='text-sm font-bold'>Unpublish</p>
-                                </div>
-                            </ToolTip>
+                        <div className='flex flex-col justify-center antialiased'>
+                            <div className='flex mx-auto mt-5 items-center space-x-4'>
+                                <div className='flex bg-green-200/60 text-xs rounded-full px-3.5 py-2 mx-auto font-bold outline outline-1 outline-green-300'>Published</div>
+                                <div><EllipsisVertical className='text-gray-500' size={13} /></div>
+                                <ToolTip
+                                    side='left'
+                                    slateBlack
+                                    sideOffset={10}
+                                    content="Make your Assignment public and available for students" >
+                                    <div className='flex px-3 py-2 cursor-pointer rounded-md space-x-2 items-center bg-gradient-to-bl text-green-800 font-medium from-green-400/50 to-lime-200/80  border border-green-600/10 shadow-green-900/10 shadow-lg'>
+                                        <BookOpen size={18} />
+                                        <p className=' text-sm font-bold'>Publish</p>
+                                    </div>
+                                </ToolTip>
+                                <ToolTip
+                                    side='left'
+                                    slateBlack
+                                    sideOffset={10}
+                                    content="Make your Assignment unavailable for students" >
+                                    <div className='flex px-3 py-2 cursor-pointer rounded-md space-x-2 items-center bg-gradient-to-bl text-gray-800 font-medium from-gray-400/50 to-gray-200/80 border border-gray-600/10 shadow-gray-900/10 shadow-lg'>
+                                        <BookX size={18} />
+                                        <p className='text-sm font-bold'>Unpublish</p>
+                                    </div>
+                                </ToolTip>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="flex h-full w-full">
-                <AssignmentProvider assignment_uuid={'assignment_' + params.assignmentuuid}>
+                <div className="flex h-full w-full">
+
                     <AssignmentsTaskProvider>
                         <div className='flex w-[400px] flex-col h-full custom-dots-bg'>
                             <div className='flex mx-auto px-3.5 py-1 bg-neutral-600/80 space-x-2 my-5 items-center text-sm font-bold text-white rounded-full'>
@@ -65,10 +66,21 @@ function AssignmentEdit() {
                             </AssignmentProvider>
                         </div>
                     </AssignmentsTaskProvider>
-                </AssignmentProvider>
-            </div>
+                </div>
+            </AssignmentProvider>
         </div>
     )
 }
 
 export default AssignmentEdit
+
+function BrdCmpx() {
+    const assignment = useAssignments() as any
+
+    useEffect(() => {
+    }, [assignment])
+
+    return (
+        <BreadCrumbs type="assignments" last_breadcrumb={assignment?.assignment_object?.title} />
+    )
+}
