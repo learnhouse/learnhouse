6a7e33f12729abbb92c71d802c0538065d94391e
swve <bdswve@gmail.com>
2024-08-29 19:40:17 +0200

    fix: add cascades for foreign keys

--- apps/api/src/db/courses/chapter_activities.py
+++ apps/api/src/db/courses/chapter_activities.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from sqlalchemy import BigInteger, Column, ForeignKey
+from sqlalchemy import BigInteger, Column, ForeignKey, Integer
 from sqlmodel import Field, SQLModel
 
 class ChapterActivity(SQLModel, table=True):
@@ -8,6 +8,8 @@ class ChapterActivity(SQLModel, table=True):
     chapter_id: int = Field(sa_column=Column(BigInteger, ForeignKey("chapter.id", ondelete="CASCADE")))
     activity_id: int = Field(sa_column=Column(BigInteger, ForeignKey("activity.id", ondelete="CASCADE")))
     course_id : int = Field(sa_column=Column(BigInteger, ForeignKey("course.id", ondelete="CASCADE")))
-    org_id : int = Field(default=None, foreign_key="organization.id")
+    org_id: int = Field(
+        sa_column=Column(Integer, ForeignKey("organization.id", ondelete="CASCADE"))
+    )
     creation_date: str
     update_date: str
