de9be129acb19b38aab5d2dc175a83f6d32e4757
swve <bdswve@gmail.com>
2024-10-10 22:44:56 +0200

    feat: add buttons extension blocks

---
+++ apps/web/components/Objects/Editor/Extensions/Buttons/Buttons.ts
@@ -0,0 +1,43 @@
+import { ReactNodeViewRenderer } from "@tiptap/react";
+import { mergeAttributes, Node } from "@tiptap/core";
+import ButtonsExtension from "./ButtonsExtension";
+
+export default Node.create({
+  name: "button",
+  group: "block",
+  draggable: true,
+  content: "text*",
+
+  addAttributes() {
+    return {
+      emoji: {
+        default: '🔗',
+      },
+      link: {
+        default: '',
+      },
+      color: {
+        default: 'blue',
+      },
+      alignment: {
+        default: 'left',
+      },
+    };
+  },
+
+  parseHTML() {
+    return [
+      {
+        tag: "button-block",
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ["button-block", mergeAttributes(HTMLAttributes), 0];
+  },
+
+  addNodeView() {
+    return ReactNodeViewRenderer(ButtonsExtension);
+  },
+});
