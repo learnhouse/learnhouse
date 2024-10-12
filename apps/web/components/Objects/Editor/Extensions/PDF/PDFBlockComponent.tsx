69468d34c8811d0fad6875b448b8d8cf414aafb6
Chris Holland <chrishollandaise@gmail.com>
2024-10-12 13:55:13 -0700

    refactor/fix: lift file select logic into its own component block

--- apps/web/components/Objects/Editor/Extensions/PDF/PDFBlockComponent.tsx
+++ apps/web/components/Objects/Editor/Extensions/PDF/PDFBlockComponent.tsx
@@ -1,14 +1,14 @@
 import { NodeViewWrapper } from '@tiptap/react'
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import { AlertTriangle, FileText, Loader } from 'lucide-react'
+import { AlertTriangle, FileText } from 'lucide-react'
 import { uploadNewPDFFile } from '../../../../../services/blocks/Pdf/pdf'
-import { UploadIcon } from '@radix-ui/react-icons'
 import { getActivityBlockMediaDirectory } from '@services/media/media'
 import { useOrg } from '@components/Contexts/OrgContext'
 import { useCourse } from '@components/Contexts/CourseContext'
 import { useEditorProvider } from '@components/Contexts/Editor/EditorContext'
 import { useLHSession } from '@components/Contexts/LHSessionContext'
+import { FileUploadBlock, FileUploadBlockButton, FileUploadBlockInput } from '../../FileUploadBlock'
 
 function PDFBlockComponent(props: any) {
   const org = useOrg() as any
@@ -48,39 +48,11 @@ function PDFBlockComponent(props: any) {
 
   return (
     <NodeViewWrapper className="block-pdf">
-      {!blockObject && (
-        <BlockPDFWrapper
-          className="flex items-center space-x-3 py-7 bg-gray-50 rounded-xl text-gray-900 px-3 border-dashed border-gray-150 border-2"
-          contentEditable={isEditable}
-        >
-          {isLoading ? (
-            <Loader
-              className="animate-spin animate-pulse text-gray-200"
-              size={50}
-            />
-          ) : (
-            <>
-              <div>
-                <FileText className="text-gray-200" size={50} />
-              </div>
-              <input
-                className="p-3 rounded-lg file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 hover:file:cursor-pointer  file:bg-gray-200 cursor-pointer file:text-gray-500"
-                onChange={handlePDFChange}
-                type="file"
-                name=""
-                id=""
-              />
-              <button
-                className="p-2 px-3 bg-gray-200 rounded-lg text-gray-500 hover:bg-gray-300 transition space-x-2 items-center flex"
-                onClick={handleSubmit}
-              >
-                <UploadIcon></UploadIcon>
-                <p>Submit</p>
-              </button>
-            </>
-          )}
-        </BlockPDFWrapper>
-      )}
+      <FileUploadBlock isEditable={isEditable} isLoading={isLoading} isEmpty={!blockObject} Icon={FileText}>
+        <FileUploadBlockInput onChange={handlePDFChange} accept="application/pdf" />
+        <FileUploadBlockButton onClick={handleSubmit} disabled={!pdf}/>
+      </FileUploadBlock>
+      
       {blockObject && (
         <BlockPDF>
           <iframe
@@ -106,14 +78,6 @@ function PDFBlockComponent(props: any) {
 }
 
 export default PDFBlockComponent
-
-const BlockPDFWrapper = styled.div`
-  // center
-  align-items: center;
-  justify-content: center;
-  text-align: center;
-  font-size: 14px;
-`
 
 const BlockPDF = styled.div`
   display: flex;
