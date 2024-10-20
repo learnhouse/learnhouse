abdc7113d793f64094f7973c4dff65cc1e8638a9
swve <bdswve@gmail.com>
2024-10-20 23:12:33 +0200

    feat: make external pages responsive

--- apps/web/components/StyledElements/Wrappers/GeneralWrapper.tsx
+++ apps/web/components/StyledElements/Wrappers/GeneralWrapper.tsx
@@ -1,6 +1,6 @@
 function GeneralWrapperStyled({ children }: { children: React.ReactNode }) {
   return (
-    <div className="max-w-screen-2xl mx-auto px-16 py-5 tracking-tight z-50">
+    <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 py-5 tracking-tight z-50">
       {children}
     </div>
   )
