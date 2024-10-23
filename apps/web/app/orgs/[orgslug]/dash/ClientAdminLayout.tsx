3014817d957773a04a4aa97b735f0d6017a154b2
swve <bdswve@gmail.com>
2024-10-23 12:03:45 +0200

    feat: add mobile dashboard menu, make dashboard pages responsive

--- apps/web/app/orgs/[orgslug]/dash/ClientAdminLayout.tsx
+++ apps/web/app/orgs/[orgslug]/dash/ClientAdminLayout.tsx
@@ -1,8 +1,10 @@
 'use client';
-import LeftMenu from '@components/Dashboard/UI/LeftMenu'
+import DashLeftMenu from '@components/Dashboard/UI/DashLeftMenu'
+import DashMobileMenu from '@components/Dashboard/UI/DashMobileMenu'
 import AdminAuthorization from '@components/Security/AdminAuthorization'
 import { SessionProvider } from 'next-auth/react'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useMediaQuery } from 'usehooks-ts';
 
 function ClientAdminLayout({
     children,
@@ -11,11 +13,17 @@ function ClientAdminLayout({
     children: React.ReactNode
     params: any
 }) {
+    const isMobile = useMediaQuery('(max-width: 768px)')
+
     return (
         <SessionProvider>
             <AdminAuthorization authorizationMode="page">
-                <div className="flex">
-                    <LeftMenu />
+                <div className="flex flex-col md:flex-row">
+                    {isMobile ? (
+                        <DashMobileMenu />
+                    ) : (
+                        <DashLeftMenu />
+                    )}
                     <div className="flex w-full">{children}</div>
                 </div>
             </AdminAuthorization>
