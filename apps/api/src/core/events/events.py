0da53fd2fa27a58d8482750a728dac2de29f0c09
swve <bdswve@gmail.com>
2025-03-12 13:14:24 +0100

    fix: remove sentry from backend

--- apps/api/src/core/events/events.py
+++ apps/api/src/core/events/events.py
@@ -5,7 +5,6 @@ from src.core.events.autoinstall import auto_install
 from src.core.events.content import check_content_directory
 from src.core.events.database import close_database, connect_to_db
 from src.core.events.logs import create_logs_dir
-from src.core.events.sentry import init_sentry
 
 
 def startup_app(app: FastAPI) -> Callable:
@@ -13,9 +12,6 @@ def startup_app(app: FastAPI) -> Callable:
         # Get LearnHouse Config
         learnhouse_config: LearnHouseConfig = get_learnhouse_config()
         app.learnhouse_config = learnhouse_config  # type: ignore
-
-        # Init Sentry
-        await init_sentry(app)
 
         # Connect to database
         await connect_to_db(app)
