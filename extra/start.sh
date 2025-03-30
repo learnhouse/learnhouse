7cb986e86a375cd573f008f0e5dcbc130844d094
Meirbek-dev <meirbek.dev@gmail.com>
2025-03-30 15:08:39 +0500

    upgraded docker and backend dependencies

--- extra/start.sh
+++ extra/start.sh
@@ -2,7 +2,7 @@
 
 # Start the services
 pm2 start server.js --cwd /app/web --name learnhouse-web > /dev/null 2>&1
-pm2 start python3 app.py --cwd /app/api --name learnhouse-api > /dev/null 2>&1
+pm2 start app.py --cwd /app/api --name learnhouse-api > /dev/null 2>&1
 
 # Check if the services are running and log the status
 pm2 status
