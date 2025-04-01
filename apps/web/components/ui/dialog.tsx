9e7a87150428dc2e1b61ebe31c5aed95c852fd5e
Meirbek-dev <meirbek.dev@gmail.com>
2025-04-01 19:17:26 +0500

    tweaked shadcn components

--- apps/web/components/ui/dialog.tsx
+++ apps/web/components/ui/dialog.tsx
@@ -76,7 +76,7 @@ function DialogHeader({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="dialog-header"
-      className={cn('flex flex-col gap-2 text-center sm:text-left', className)}
+      className={cn('flex flex-col gap-1 text-center sm:text-left', className)}
       {...props}
     />
   )
@@ -87,7 +87,7 @@ function DialogFooter({ className, ...props }: React.ComponentProps<'div'>) {
     <div
       data-slot="dialog-footer"
       className={cn(
-        'flex flex-col-reverse gap-2 sm:flex-row sm:justify-end',
+        'flex flex-col-reverse gap-1 sm:flex-row sm:justify-end',
         className
       )}
       {...props}
