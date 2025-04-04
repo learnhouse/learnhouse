ac55d4d0f8a97296516755d42cfb5ec7f8292745
Meirbek-dev <Meirbek.dev@gmail.com>
2025-04-04 21:15:15 +0500

    chore: upgraded to chromadb 1.0

--- apps/web/components/Objects/StyledElements/Form/TagInput.tsx
+++ apps/web/components/Objects/StyledElements/Form/TagInput.tsx
@@ -1,5 +1,5 @@
 import { type Tag, TagInput } from 'emblor'
-import type { type Dispatch, type SetStateAction } from 'react'
+import type { Dispatch, SetStateAction } from 'react'
 import { useState, useEffect } from 'react'
 
 interface FormTagInputProps {
