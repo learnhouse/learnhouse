31c557ee07de58e13f628f84cedc6871d7bc63fa
swve <bdswve@gmail.com>
2025-08-06 14:45:08 +0200

    fix: make logfire disabled by default
    Initially started by
    https://github.com/learnhouse/learnhouse/pull/496

--- apps/api/src/core/events/database.py
+++ apps/api/src/core/events/database.py
@@ -58,7 +58,7 @@ else:
 # Only create tables if not in test mode (tests will handle this themselves)
 if not is_testing:
     SQLModel.metadata.create_all(engine)
-    logfire.instrument_sqlalchemy(engine=engine)
+    # Note: logfire instrumentation will be handled in app.py after configuration
 
 async def connect_to_db(app: FastAPI):
     app.db_engine = engine  # type: ignore
