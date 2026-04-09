70e5553471eab23bbe482af5d59ed0575a7cb24f
swve <bdswve@gmail.com>
2026-04-09 12:06:19 +0200

    release: bump version to 1.1.0

--- apps/api/ee/routers/info.py
+++ apps/api/ee/routers/info.py
@@ -7,7 +7,7 @@ async def get_ee_status():
     """Return the status of the Enterprise Edition."""
     return {
         "enabled": True,
-        "version": "1.0.1",
+        "version": "1.1.0",
         "license": "enterprise"
     }
 
