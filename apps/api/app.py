a8110444d79ffb72065480bdb30c09a83a541054
swve <bdswve@gmail.com>
2026-09-06 21:04:05 +0200

    release: bump version to 1.3.6

--- apps/api/app.py
+++ apps/api/app.py
@@ -143,7 +143,7 @@ app = FastAPI(
     description=learnhouse_config.site_description,
     docs_url="/docs" if learnhouse_config.general_config.development_mode else None,
     redoc_url="/redoc" if learnhouse_config.general_config.development_mode else None,
-    version="1.3.5",
+    version="1.3.6",
     lifespan=lifespan,
 )
 
